4f84edd0fa90aa984576f9052259d392b11601eb
bhavsingh2017 <bhavkirs@usc.edu>
2019-04-21 18:49:28 -0700

    just docker

--- django_file/Camino_Form/Camino_Form/settings.py
+++ django_file/Camino_Form/Camino_Form/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 't2n+#s55qm#8)d4&dq&y_p6cg*$6_3d$&+s)gptnp(b#cxyh(h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+#ALLOWED_HOSTS = [‘0.0.0.0’, ‘localhost’, ‘camino-test-bhav.herokuapp.com’]
+ALLOWED_HOSTS=[]
 
 # Application definition
 
@@ -117,5 +117,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+#STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
 STATIC_URL = '/static/'
